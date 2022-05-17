dee4182486978ae98b05c0c8b1f66d5271b8f722
SebastianSossa <jssossa2@misena.edu.co>
2022-05-16 19:11:07 -0500

    cambio1

--- p.js
+++ p.js
@@ -1,6 +1,6 @@
 function qr() {
     var t = document.getElementById("text").value;
-    document.getElementById(
+    document.getElemen
       "image"
     ).innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${t}"></img>`;
   }
